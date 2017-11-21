5b4ffc2624ba034037166856ed3415c28a7fbf79
Anze <anze@grafolean.com>
2017-11-21 22:29:10 +0100

    Add charting libraries

--- frontend/src/components/Chart/index.js
+++ frontend/src/components/Chart/index.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+
+const chartData = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+}
 
 class Chart extends Component {
   render() {
     return (
       <div className="chart">
-        asdf
+        <Line data={chartData} width="600" height="250"/>
       </div>
     );
   }
