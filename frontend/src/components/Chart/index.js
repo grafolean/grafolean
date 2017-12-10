1d7838dd97b1921a4fee83031ec3f2b53d8afbb5
Anze <anze@grafolean.com>
2017-12-10 15:16:59 +0100

    Use recharts instead of charts.js; add test chart for aggregated data

--- frontend/src/components/Chart/index.js
+++ frontend/src/components/Chart/index.js
@@ -1,39 +1,23 @@
 import React, { Component } from 'react';
-import { Line } from 'react-chartjs-2';
+import { LineChart, Line, AreaChart, Area } from 'recharts';
 
-const chartData = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-}
+const data = [
+  {name: 'Page A', uv: 4000, pv: 2430, amt: [2400,2500]},
+  {name: 'Page B', uv: 3000, pv: 2398, amt: [2210,2600]},
+  {name: 'Page C', uv: 2000, pv: 2340, amt: [2290,2550]},
+  {name: 'Page D', uv: 2780, pv: 2108, amt: [2000,2200]},
+  {name: 'Page E', uv: 1890, pv: 2200, amt: [2181,2300]},
+  {name: 'Page F', uv: 2390, pv: 2600, amt: [2500,2700]},
+  {name: 'Page G', uv: 3490, pv: 2120, amt: [2100,2150]},
+];
 
 class Chart extends Component {
   render() {
     return (
-      <div className="chart">
-        <Line data={chartData} width="600" height="250"/>
-      </div>
+      <AreaChart width={400} height={400} data={data}>
+        <Area type="linear" dataKey="pv" stroke="#ff6600" fillOpacity={0} fill="#ff6600" />
+        <Area type="linear" dataKey="amt" stroke={false} fillOpacity={0.1} fill="#ff6600" />
+      </AreaChart>
     );
   }
 }
