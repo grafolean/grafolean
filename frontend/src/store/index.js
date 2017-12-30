2b823435e61c93cb28ebe129a5b300459e5e2463
Anze <anze@grafolean.com>
2017-12-30 23:34:46 +0100

    Add support for Redux dev tools browser extension

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,6 +5,7 @@ import moonthorApp from './reducers'
 
 const store = createStore(
     moonthorApp,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(
       thunkMiddleware, // lets us dispatch() functions
     )
