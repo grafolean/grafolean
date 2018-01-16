a38ce9cbdaa72dd611529f8cc16399e5e2e02b7a
Anze <anze@grafolean.com>
2018-01-16 20:16:57 +0100

    Frontend: use props to display charts

--- frontend/src/components/Chart/index.js
+++ frontend/src/components/Chart/index.js
@@ -9,19 +9,9 @@ import {
 } from 'victory';
 import Loading from '../Loading'
 import moment from 'moment';
+import range from 'lodash/range';
 
-const timeTickFormatter = (tick) => moment(tick * 1000).format('HH:mm')
-
-/*
-props:
-  - chart id
-  - title (chart name)
-  - x domain
-  - paths (for legend)
-  - data (must include all data for x domain, but is usually wider)
-  - total data in aggr level 6 for ... navchart?
-  - y domain (if not specified, y domain will be calculated by using max value in data and min(min value, 0))
-*/
+const timeTickFormatter = (tick) => moment(tick * 1000).format('DD.MM.YY HH:mm')
 
 
 class Chart extends Component {
@@ -31,53 +21,47 @@ class Chart extends Component {
     // the goal is to provide an intelligent way of marking X axis
     return {
       tickFormat: timeTickFormatter,
-//      tickValues: [1080000000, 1080003600, 1080007200, 1080010800, 1080014400, 1080018000, 1080021600, 1080025200],
-      tickValues: [domainXFrom, domainXTo],
+      tickValues: range(domainXFrom, domainXTo, (domainXTo - domainXFrom) / 5),
     }
   }
 
   render() {
-    let domainXTickProps = this._domainXTickProps(1080000000, 1080025200)
-    console.log(domainXTickProps)
     return (
-        (!this.props.data) ? (
+        (this.props.loading) ? (
           <Loading />
         ) : (
           <VictoryChart
-            //domain={this.props.domain}
-            domain={{x: [1080000000, 1080025200], y: [0, 4500]}}
+            domain={this.props.domain}
           >
             <VictoryAxis
               {...this._domainXTickProps(this.props.domain.x[0], this.props.domain.x[1])}
-              //{...this._domainXTickProps(1080000000, 1080025200)}
-              //{...domainXTickProps}
-              //tickFormat={timeTickFormatter}
-              //tickValues={[1080000000, 1080003600, 1080007200, 1080010800, 1080014400, 1080018000, 1080021600, 1080025200]}
             />
             <VictoryAxis
               dependentAxis
               tickFormat={(tick) => `$${Math.round(tick)}M`}
             />
-            <VictoryArea
-              data={this.props.data}
-              x="t"
-              y="ymin"
-              y0="ymax"
-              style={{
-                data: {
-                  fill: "#c43a31", fillOpacity: 0.2, strokeWidth: 0
-                },
-              }}
-            />
-            <VictoryLine
-              data={this.props.data}
-              x="t"
-              y="y"
-              style={{
-                data: { stroke: "#c43a31", strokeWidth: 1 },
-                parent: { border: "1px solid #ccc"},
-              }}
-            />
+
+            {Object.keys(this.props.series).map((path) => {
+              return ([
+                <VictoryArea
+                  data={this.props.series[path].data}
+                  x="t" y="ymin" y0="ymax"
+                  style={{
+                    data: {
+                      fill: "#c43a31", fillOpacity: 0.2, strokeWidth: 0
+                    },
+                  }}
+                />,
+                <VictoryLine
+                  data={this.props.series[path].data}
+                  x="t" y="y"
+                  style={{
+                    data: { stroke: "#c43a31", strokeWidth: 1 },
+                    parent: { border: "1px solid #ccc"},
+                  }}
+                />
+              ])
+            })}
         </VictoryChart>
       )
 
--- frontend/src/containers/ChartContainer.js
+++ frontend/src/containers/ChartContainer.js
@@ -2,41 +2,53 @@ import { connect } from 'react-redux'
 import Chart from '../components/Chart'
 
 const mapStateToProps = (state, ownProps) => {
-  const defaultProps = {
-    chartId: ownProps.chartId,
-    name: ownProps.name,
-    paths: ownProps.paths,
+
+  const exampleProps = {
+    chartId: 1,
+    name: "This is chart title",
     domain: {
       x: [
-        Math.floor(Date.now() / 1000) - 3600*24*30,  // last 30 days by default
-        Math.floor(Date.now() / 1000),
+        // Math.floor(Date.now() / 1000) - 3600*24*30,  // last 30 days by default
+        // Math.floor(Date.now() / 1000),
+        1080000000, 1080024000
       ],
-      y: null,  // [ 0, 4500 ]
+      y: [ 0, 4500 ],
     },
-    isAggregated: false,
-    data: [
-      //{t: 1080001800, y: 3413},  // if isAggregated is false
-      //{t: 1080001800, y: 3413, ymin: 3400, ymax: 3500},  // if isAggregated is true
-    ],
+    isAggregated: true,
+    loading: false,
+    series: {
+      "aaa.bbb.ccc.111": {
+        name: "Path 1",
+        data: [
+          {t: 1080001800, y: 3413, ymin: 3400, ymax: 3500},
+          {t: 1080005400, y: 1398, ymin: 1210, ymax: 1600},
+          {t: 1080009000, y: 2340, ymin: 2290, ymax: 2550},
+          {t: 1080012600, y: 2108, ymin: 2000, ymax: 2200},
+          {t: 1080016200, y: 2200, ymin: 2181, ymax: 2300},
+          {t: 1080019800, y: 2600, ymin: 2500, ymax: 2700},
+          {t: 1080023400, y: 2120, ymin: 2100, ymax: 2150},
+        ],
+      },
+      "aaa.bbb.ccc.222": {
+        name: "Path 2",
+        data: [
+          {t: 1080001800, y: 2413, ymin: 2413, ymax: 2413},
+          {t: 1080005400, y: 3398, ymin: 3398, ymax: 3398},
+          {t: 1080009000, y: 2240, ymin: 2240, ymax: 2240},
+          {t: 1080012600, y: 2308, ymin: 2308, ymax: 2308},
+          {t: 1080016200, y: 1500, ymin: 1500, ymax: 1500},
+          {t: 1080019800, y: 2200, ymin: 2200, ymax: 2200},
+          {t: 1080023400, y: 1720, ymin: 1720, ymax: 1720},
+        ],
+      },
+    },
   }
 
-    console.log("STATE", state);
-    return {
-      ...defaultProps,
-      data: [
-        {t: 1080001800, y: 3413, ymin: 3400, ymax: 3500},
-        {t: 1080005400, y: 1398, ymin: 1210, ymax: 1600},
-        {t: 1080009000, y: 2340, ymin: 2290, ymax: 2550},
-        {t: 1080012600, y: 2108, ymin: 2000, ymax: 2200},
-        {t: 1080016200, y: 2200, ymin: 2181, ymax: 2300},
-        {t: 1080019800, y: 2600, ymin: 2500, ymax: 2700},
-        {t: 1080023400, y: 2120, ymin: 2100, ymax: 2150},
-      ],
-    }
-  }
+  return exampleProps;
+}
 
-  const ChartContainer = connect(
-    mapStateToProps,
-  )(Chart)
+const ChartContainer = connect(
+  mapStateToProps,
+)(Chart)
 
-  export default ChartContainer
+export default ChartContainer
