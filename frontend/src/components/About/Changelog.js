a5230ff78cab65ee61bc9d18f94457aa2e6137d8
Anze <anze@grafolean.com>
2019-01-23 21:40:57 +0100

    Add dates to changelog

--- frontend/src/components/About/Changelog.js
+++ frontend/src/components/About/Changelog.js
@@ -1,14 +1,18 @@
 import changelogJson from '../../CHANGELOG.json';
+import moment from 'moment';
 
 import React from 'react';
 
 export default class Changelog extends React.PureComponent {
   render() {
     return (
-      <div>
+      <div className="changelog">
         {changelogJson.map(entry => (
-          <div className="entry">
-            <h3>{entry.version}</h3>
+          <div className="entry" key={entry.version}>
+            <h3>
+              {entry.version}{' '}
+              <span className="created-at">({moment(entry.created_at).format('YYYY-MM-DD')})</span>
+            </h3>
             <pre>{entry.changelog}</pre>
           </div>
         ))}
