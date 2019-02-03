5ed142bfeb11c644f93c07c9646db8944bb1c1ef
Anze <anze@grafolean.com>
2019-02-03 11:10:40 +0100

    Fix MQTT websockets (ws://, not yet wss://, JWT auth compiled but not enabled)

--- frontend/src/components/WelcomePage/index.js
+++ frontend/src/components/WelcomePage/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 export default class WelcomePage extends React.PureComponent {
   render() {
-    // const padding = 40;
-    // const halfColumnWidth = this.props.innerWidth / 2 - padding;
     return (
       <div>
         <h3>Welcome!</h3>
@@ -11,16 +9,45 @@ export default class WelcomePage extends React.PureComponent {
           This page displays information about the latest values received and provides guidance on how to post
           these values.
         </p>
-        {/* <GLeanChartWidget
-          width={halfColumnWidth}
-          height={400}
-          widgetId={'number-of-last-values-chart'}
-          dashboardSlug={null}
-          title="Number of posted values"
-          chartContent={[]}
-          onWidgetDelete={this.fetchDashboardDetails}
-        /> */}
+        {/* <WebSocketsTest address="wss://echo.websocket.org" /> */}
+        <WebSocketsTest address="ws://localhost:9883" />
       </div>
     );
+  }
+}
+
+class WebSocketsTest extends React.PureComponent {
+  ws = undefined;
+
+  componentDidMount() {
+    this.ws = new WebSocket(this.props.address);
+    this.ws.onopen = this.onSocketOpen;
+    this.ws.onmessage = this.onSocketMessage;
+    this.ws.onclose = this.onSocketClose;
+  }
+
+  componentWillUnmount() {
+    if (this.ws) {
+      this.ws.close();
+    }
+  }
+
+  onSocketOpen = () => {
+    console.log('Socket opened');
+    this.ws.send('test123');
+    console.log('Message is sent...');
+  };
+
+  onSocketMessage = evt => {
+    var received_msg = evt.data;
+    console.log('Message received:', received_msg);
+  };
+
+  onSocketClose = () => {
+    console.log('Connection is closed...');
+  };
+
+  render() {
+    return <div>See console for messages.</div>;
   }
 }
