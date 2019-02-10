b211f7ac72c1f60be959bbf401d1fc76259cf2c8
Anze <anze@grafolean.com>
2019-02-10 14:02:23 +0100

    Warn API users of common pitfalls

--- backend/grafolean.py
+++ backend/grafolean.py
@@ -47,6 +47,9 @@ def before_request():
     flask.g.grafolean_data = {}
 
     if not flask.request.endpoint in app.view_functions:
+        # Calling /api/admin/... is a common mistake, so it deserves a warning in the log:
+        if flask.request.path[:11] == '/api/admin/' and flask.request.method == 'GET':
+            log.warning("Did you want to use POST instead of GET?")
         return "Resource not found", 404
 
     # Browser might (if frontend and backend are not on the same origin) send a pre-flight OPTIONS request to get the
@@ -143,7 +146,7 @@ def after_request(response):
 
 @app.errorhandler(ValidationError)
 def handle_invalid_usage(error):
-    return str(error), 400
+    return f'Input validation failed: {str(error)}', 400
 
 @app.errorhandler(Exception)
 def handle_error(e):
