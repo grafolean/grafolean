639140c8e103bd1635ba290c120ff57a58a42898
Anze <anze@grafolean.com>
2019-02-17 21:14:09 +0100

    Use docker-compose to setup integration testing environment

--- backend/tests/integration.py
+++ backend/tests/integration.py
@@ -2,6 +2,15 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# since we are running our own environment for testing (use `docker-compose up -d` and
+# `docker-compose down`), we need to setup env vars accordingly:
+os.environ['DB_HOST'] = 'localhost'  # we expose to port 5432 on host (== localhost)
+os.environ['DB_DATABASE'] = 'pytest'
+os.environ['DB_USERNAME'] = 'pytest'
+os.environ['DB_PASSWORD'] = 'pytest'
+# os.environ['MQTT_HOSTNAME'] = 'localhost'
+# os.environ['MQTT_PORT'] = '1883'
+
 import json
 import pytest
 from pprint import pprint
