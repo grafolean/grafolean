82bff6e19591b293cf32ebce08b59fcb4d17b2fb
Anze <anze@grafolean.com>
2019-03-19 20:51:15 +0100

    Move marks

--- frontend/src/components/Widgets/GLeanChartWidget/YAxisMinMaxAdjuster.js
+++ frontend/src/components/Widgets/GLeanChartWidget/YAxisMinMaxAdjuster.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
 class AdjusterMark extends React.PureComponent {
-  state = {
-    y: this.props.y,
-  };
+  initialY = this.props.y;
   dragging = false;
 
   startDrag = ev => {
     this.dragging = true;
     this.initialEventY = ev.pageY;
-
     // make sure we follow the pointermove events, even if they are not over our component:
     ev.target.setPointerCapture(ev.pointerId);
   };
@@ -23,20 +20,16 @@ class AdjusterMark extends React.PureComponent {
       return;
     }
     const diffY = ev.pageY - this.initialEventY;
-    this.setState({
-      dragging: false,
-      y: this.props.y + diffY,
-    });
+    this.props.onChange(this.initialY + diffY);
   };
 
   render() {
-    const { x } = this.props;
-    const { y } = this.state;
-    const A = 4;
-    const B = 2;
+    const { x, y } = this.props;
+    const A = 4; // half of mark height
+    const B = 2; // width of rectangle
     return (
       <path
-        d={`M${x},${y} l${-A},${A} l${-B},0 l0,${-2 * A} l${+B - 1 /* why -1? who knows... */},0 l${A},${A}`}
+        d={`M${x},${y} l${-A},${A} l${-B},0 l0,${-2 * A} l${+B},0 l${A},${A}`}
         onPointerDown={this.startDrag}
         onPointerUp={this.endDrag}
         onPointerCancel={this.endDrag}
@@ -47,12 +40,58 @@ class AdjusterMark extends React.PureComponent {
 }
 
 export default class YAxisMinMaxAdjuster extends React.PureComponent {
+  static defaultProps = {
+    minYDiff: 10,
+    defaultMinYValue: 0,
+    defaultMaxYValue: 10,
+    v2y: v => null,
+    x: 0,
+  };
+  state = {
+    minY: this.props.v2y(this.props.defaultMinYValue),
+    maxY: this.props.v2y(this.props.defaultMaxYValue),
+    adjusting: false,
+  };
+
+  adjustStart = () => {
+    this.setState({ adjusting: true });
+  };
+  adjustEnd = () => {
+    this.setState({ adjusting: false });
+  };
+  changeMinY = y => {
+    const { v2y, defaultMinYValue, minYDiff } = this.props;
+    this.setState(({ maxY }) => ({
+      minY: Math.min(v2y(defaultMinYValue), Math.max(maxY + minYDiff, y)),
+    }));
+  };
+  changeMaxY = y => {
+    const { v2y, defaultMaxYValue, minYDiff } = this.props;
+    this.setState(({ minY }) => ({
+      maxY: Math.max(v2y(defaultMaxYValue), Math.min(minY - minYDiff, y)),
+    }));
+  };
+
   render() {
-    const { x, v2y, defaultMinYValue, defaultMaxYValue } = this.props;
+    const { x } = this.props;
+    const { minY, maxY } = this.state;
+
     return (
       <g className="yaxis-minmax-adjuster">
-        <AdjusterMark x={Math.round(x)} y={Math.round(v2y(defaultMinYValue))} />
-        <AdjusterMark x={Math.round(x)} y={Math.round(v2y(defaultMaxYValue))} />
+        <AdjusterMark
+          x={x}
+          y={minY}
+          onDragStart={this.adjustStart}
+          onChange={this.changeMinY}
+          onDragEnd={this.adjustEnd}
+        />
+        <AdjusterMark
+          x={x}
+          y={maxY}
+          onDragStart={this.adjustStart}
+          onChange={this.changeMaxY}
+          onDragEnd={this.adjustEnd}
+        />
       </g>
     );
   }
