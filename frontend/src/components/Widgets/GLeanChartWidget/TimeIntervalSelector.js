69842aff746485cc71a1b0796e1fb35009cdc215
Anze <anze@grafolean.com>
2019-04-25 16:33:21 +0200

    Add TimeIntervalSelector buttons

---
+++ frontend/src/components/Widgets/GLeanChartWidget/TimeIntervalSelector.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import moment from 'moment';
+
+import Button from '../../Button';
+
+export default class TimeIntervalSelector extends React.PureComponent {
+  static defaultProps = {
+    options: {
+      '1y': moment.duration(1, 'year'),
+      '1m': moment.duration(1, 'month'),
+      '7d': moment.duration(7, 'day'),
+      '24h': moment.duration(24, 'hour'),
+      '12h': moment.duration(12, 'hour'),
+      '3h': moment.duration(3, 'hour'),
+      '30min': moment.duration(30, 'minute'),
+    },
+  };
+
+  handleButtonClick = ev => {
+    ev.preventDefault();
+    const optionKey = ev.target.attributes.datakey.value;
+    this.props.onChange(this.props.options[optionKey]);
+  };
+
+  render() {
+    const { options, style } = this.props;
+    return (
+      <div className="time-interval-selector" style={style}>
+        Last:
+        {Object.keys(options).map(k => (
+          <Button key={k} datakey={k} onClick={this.handleButtonClick}>
+            {k}
+          </Button>
+        ))}
+      </div>
+    );
+  }
+}
--- frontend/src/components/Widgets/GLeanChartWidget/index.js
+++ frontend/src/components/Widgets/GLeanChartWidget/index.js
@@ -20,6 +20,7 @@ import isWidget from '../isWidget';
 import { fetchAuth } from '../../../utils/fetch';
 import ChartTooltipPopup from './ChartTooltipPopup';
 import YAxisMinMaxAdjuster from './YAxisMinMaxAdjuster';
+import TimeIntervalSelector from './TimeIntervalSelector';
 
 class GLeanChartWidget extends React.Component {
   state = {
@@ -124,6 +125,10 @@ class GLeanChartWidget extends React.Component {
     });
   };
 
+  handleTimeIntervalSelect = intervalDuration => {
+    console.log({ intervalDuration });
+  };
+
   render() {
     const MAX_YAXIS_WIDTH = 70;
     let legendWidth, chartWidth, legendIsDockable, legendPositionStyle;
@@ -167,54 +172,59 @@ class GLeanChartWidget extends React.Component {
           width: this.props.width,
         }}
       >
-        <div>
-          <RePinchy
-            width={this.props.width}
-            height={this.props.height}
-            activeArea={{
-              x: yAxesWidth,
-              y: 0,
-              w: chartWidth - yAxesWidth,
-              h: this.props.height,
-            }}
-            kidnapScroll={this.props.isFullscreen}
-            initialState={{
-              x: initialPanX,
-              y: 0.0,
-              scale: initialScale,
-            }}
-            handleMouseMove={this.handleRePinchyMouseMove}
-            handleClick={this.handleRePinchyClick}
-          >
-            {(x, y, scale, zoomInProgress, pointerPosition) => (
-              <div className="repinchy-content">
-                <ChartContainer
-                  chartSeries={this.state.allChartSeries}
-                  drawnChartSeries={this.state.drawnChartSeries}
-                  width={chartWidth}
+        <RePinchy
+          width={this.props.width}
+          height={this.props.height}
+          activeArea={{
+            x: yAxesWidth,
+            y: 0,
+            w: chartWidth - yAxesWidth,
+            h: this.props.height,
+          }}
+          kidnapScroll={this.props.isFullscreen}
+          initialState={{
+            x: initialPanX,
+            y: 0.0,
+            scale: initialScale,
+          }}
+          handleMouseMove={this.handleRePinchyMouseMove}
+          handleClick={this.handleRePinchyClick}
+        >
+          {(x, y, scale, zoomInProgress, pointerPosition) => (
+            <div className="repinchy-content">
+              <ChartContainer
+                chartSeries={this.state.allChartSeries}
+                drawnChartSeries={this.state.drawnChartSeries}
+                width={chartWidth}
+                height={this.props.height}
+                fromTs={Math.round(-(x - yAxesWidth) / scale)}
+                toTs={Math.round(-(x - yAxesWidth) / scale) + Math.round(chartWidth / scale)}
+                scale={scale}
+                zoomInProgress={zoomInProgress}
+                xAxisHeight={xAxisHeight}
+                yAxisWidth={yAxisWidth}
+                registerMouseMoveHandler={this.registerRePinchyMouseMoveHandler}
+                registerClickHandler={this.registerRePinchyClickHandler}
+              />
+              <div style={legendPositionStyle}>
+                <Legend
+                  dockingEnabled={legendIsDockable}
+                  width={legendWidth}
                   height={this.props.height}
-                  fromTs={Math.round(-(x - yAxesWidth) / scale)}
-                  toTs={Math.round(-(x - yAxesWidth) / scale) + Math.round(chartWidth / scale)}
-                  scale={scale}
-                  zoomInProgress={zoomInProgress}
-                  xAxisHeight={xAxisHeight}
-                  yAxisWidth={yAxisWidth}
-                  registerMouseMoveHandler={this.registerRePinchyMouseMoveHandler}
-                  registerClickHandler={this.registerRePinchyClickHandler}
+                  chartSeries={this.state.allChartSeries}
+                  onDrawnChartSeriesChange={this.handleDrawnChartSeriesChange}
                 />
-                <div style={legendPositionStyle}>
-                  <Legend
-                    dockingEnabled={legendIsDockable}
-                    width={legendWidth}
-                    height={this.props.height}
-                    chartSeries={this.state.allChartSeries}
-                    onDrawnChartSeriesChange={this.handleDrawnChartSeriesChange}
-                  />
-                </div>
               </div>
-            )}
-          </RePinchy>
-        </div>
+            </div>
+          )}
+        </RePinchy>
+
+        <TimeIntervalSelector
+          style={{
+            right: legendWidth,
+          }}
+          onChange={this.handleTimeIntervalSelect}
+        />
       </div>
     );
   }
