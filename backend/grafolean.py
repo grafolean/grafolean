c53ebe30ca6b2832118cd90822746ccea41eb7fb
Anze <anze@grafolean.com>
2019-05-07 20:23:33 +0200

    Cleanup: rearrange endpoints in the file

--- backend/grafolean.py
+++ backend/grafolean.py
@@ -327,48 +327,6 @@ def admin_mqttauth_superuser(check_type):
         return "Access denied", 401
 
 
-# Useful for determining status of backend (is it available, is the first user initialized,...)
-@app.route('/api/status/info', methods=['GET'])
-@noauth
-def status_info_get():
-    db_migration_needed = utils.is_migration_needed()
-    result = {
-        'alive': True,
-        'db_migration_needed': db_migration_needed,
-        'db_version': utils.get_existing_schema_version(),
-        'user_exists': Auth.first_user_exists() if not db_migration_needed else None,
-        'cors_domains': CORS_DOMAINS,
-        'mqtt_ws_hostname': MQTT_WS_HOSTNAME,
-        'mqtt_ws_port': MQTT_WS_PORT,
-    }
-    return json.dumps(result), 200
-
-
-@app.route('/api/status/sitemap', methods=['GET'])
-@noauth
-def status_sitemap_get():
-    ignored_methods = set(['HEAD', 'OPTIONS'])
-    rules = defaultdict(set)
-    for rule in app.url_map.iter_rules():
-        rules[str(rule)] |= rule.methods
-    result = [{ 'url': k, 'methods': sorted(list(v - ignored_methods))} for k, v in rules.items()]
-    return json.dumps(result), 200
-
-
-@app.route('/api/status/cspreport', methods=['POST'])
-@noauth
-def status_cspreport():
-    log.error("CSP report received: {}".format(flask.request.data))
-    return '', 200
-
-
-@app.route('/api/profile/permissions', methods=['GET'])
-def profile_permissions():
-    user_id = flask.g.grafolean_data['user_id']
-    rec = Permission.get_list(user_id)
-    return json.dumps({'list': rec}), 200
-
-
 @app.route('/api/admin/permissions', methods=['GET', 'POST'])
 def admin_permissions_get_post():
     if flask.request.method in ['GET', 'HEAD']:
@@ -480,6 +438,58 @@ def admin_person_crud(user_id):
         if not rowcount:
             return "No such person", 404
         return "", 200
+
+
+# --------------
+# /status/ - system status information
+# --------------
+
+
+# Useful for determining status of backend (is it available, is the first user initialized,...)
+@app.route('/api/status/info', methods=['GET'])
+@noauth
+def status_info_get():
+    db_migration_needed = utils.is_migration_needed()
+    result = {
+        'alive': True,
+        'db_migration_needed': db_migration_needed,
+        'db_version': utils.get_existing_schema_version(),
+        'user_exists': Auth.first_user_exists() if not db_migration_needed else None,
+        'cors_domains': CORS_DOMAINS,
+        'mqtt_ws_hostname': MQTT_WS_HOSTNAME,
+        'mqtt_ws_port': MQTT_WS_PORT,
+    }
+    return json.dumps(result), 200
+
+
+@app.route('/api/status/sitemap', methods=['GET'])
+@noauth
+def status_sitemap_get():
+    ignored_methods = set(['HEAD', 'OPTIONS'])
+    rules = defaultdict(set)
+    for rule in app.url_map.iter_rules():
+        rules[str(rule)] |= rule.methods
+    result = [{ 'url': k, 'methods': sorted(list(v - ignored_methods))} for k, v in rules.items()]
+    return json.dumps(result), 200
+
+
+@app.route('/api/status/cspreport', methods=['POST'])
+@noauth
+def status_cspreport():
+    log.error("CSP report received: {}".format(flask.request.data))
+    return '', 200
+
+
+# --------------
+# /profile/ - user specific endpoints
+# --------------
+
+
+@app.route('/api/profile/permissions', methods=['GET'])
+def profile_permissions():
+    user_id = flask.g.grafolean_data['user_id']
+    rec = Permission.get_list(user_id)
+    return json.dumps({'list': rec}), 200
 
 
 # --------------
