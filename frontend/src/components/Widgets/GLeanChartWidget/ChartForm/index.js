ede2066e49466d7257deb8a53be68f7ac8ed0499
Anze <anze@grafolean.com>
2019-06-09 15:56:47 +0200

    Remove metric_prefix (not clearly defined what it is, not used anywhere)

--- frontend/src/components/Widgets/GLeanChartWidget/ChartForm/index.js
+++ frontend/src/components/Widgets/GLeanChartWidget/ChartForm/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import Select, { Creatable } from 'react-select';
+import { Creatable } from 'react-select';
 import 'react-select/dist/react-select.css';
 
 import MatchingPaths from './MatchingPaths';
 import Button from '../../../Button';
 import './index.scss';
 
-const METRIC_PREFIXES = [
-  { prefix: 'P', name: 'peta', power: 15 },
-  { prefix: 'T', name: 'tera', power: 12 },
-  { prefix: 'G', name: 'giga', power: 9 },
-  { prefix: 'M', name: 'mega', power: 6 },
-  { prefix: 'k', name: 'kilo', power: 3 },
-  { prefix: 'h', name: 'hecto', power: 2 },
-  { prefix: 'd', name: 'deci', power: 1 },
-  { prefix: 'c', name: 'centi', power: -2 },
-  { prefix: 'm', name: 'milli', power: -3 },
-  { prefix: 'µ', name: 'micro', power: -6 },
-  { prefix: 'n', name: 'nano', power: -9 },
-  { prefix: 'p', name: 'pico', power: -12 },
-];
+// const METRIC_PREFIXES = [
+//   { prefix: 'P', name: 'peta', power: 15 },
+//   { prefix: 'T', name: 'tera', power: 12 },
+//   { prefix: 'G', name: 'giga', power: 9 },
+//   { prefix: 'M', name: 'mega', power: 6 },
+//   { prefix: 'k', name: 'kilo', power: 3 },
+//   { prefix: 'h', name: 'hecto', power: 2 },
+//   { prefix: 'd', name: 'deci', power: 1 },
+//   { prefix: 'c', name: 'centi', power: -2 },
+//   { prefix: 'm', name: 'milli', power: -3 },
+//   { prefix: 'µ', name: 'micro', power: -6 },
+//   { prefix: 'n', name: 'nano', power: -9 },
+//   { prefix: 'p', name: 'pico', power: -12 },
+// ];
 const KNOWN_UNITS = {
   '%': { name: 'percent', allowedPrefixes: '' },
   s: { name: 'second', allowedPrefixes: 'mµnp' },
@@ -57,7 +57,6 @@ export default class ChartForm extends React.Component {
             pathRenamer: c.renaming,
             expression: c.expression,
             unit: c.unit,
-            metricPrefix: c.metric_prefix,
           }))
         : [],
     };
@@ -69,7 +68,6 @@ export default class ChartForm extends React.Component {
       renaming: sg.pathRenamer,
       expression: sg.expression,
       unit: sg.unit,
-      metric_prefix: sg.metricPrefix,
     }));
     const valid = true;
     this.props.onChange('chart', content, valid);
@@ -92,14 +90,6 @@ export default class ChartForm extends React.Component {
     };
   };
 
-  metricPrefixOptionRenderer = (pOption, unit) => (
-    <span>
-      {pOption.prefix}
-      {unit} [{pOption.name} - 10
-      <sup>{pOption.power}</sup> {unit}]
-    </span>
-  );
-
   handleAddEmptySerie = ev => {
     this.setState(prevState => ({
       seriesGroups: [
@@ -109,7 +99,6 @@ export default class ChartForm extends React.Component {
           pathRenamer: '',
           expression: '$1',
           unit: '',
-          metricPrefix: '',
         },
       ],
     }));
@@ -205,29 +194,6 @@ export default class ChartForm extends React.Component {
                   newOptionCreator={this.userUnitCreator}
                 />
               </div>
-
-              {sg.unit && (!KNOWN_UNITS[sg.unit] || KNOWN_UNITS[sg.unit].allowedPrefixes !== '') && (
-                <div className="form-item">
-                  <label>Metric prefix: (optional)</label>
-                  <Select
-                    value={sg.metricPrefix || ''}
-                    placeholder={`-- none [1${sg.unit}] --`}
-                    onChange={selectedOption =>
-                      this.setSeriesGroupProperty(sgIndex, 'metricPrefix', selectedOption.value)
-                    }
-                    options={METRIC_PREFIXES.filter(
-                      p =>
-                        !(sg.unit in KNOWN_UNITS) || KNOWN_UNITS[sg.unit].allowedPrefixes.includes(p.prefix),
-                    ).map(p => ({
-                      value: p.prefix,
-                      // no need for label because we specify optionRenderer; but we must supply additional info to it:
-                      ...p,
-                    }))}
-                    optionRenderer={pOption => this.metricPrefixOptionRenderer(pOption, sg.unit)}
-                    valueRenderer={pOption => this.metricPrefixOptionRenderer(pOption, sg.unit)}
-                  />
-                </div>
-              )}
             </div>
           ))}
           <Button onClick={this.handleAddEmptySerie}>+</Button>
