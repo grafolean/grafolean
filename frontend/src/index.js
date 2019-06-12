896de51ba5c8c3fa3a9f3dbf17f82bacca4b24de
Anze <anze@grafolean.com>
2019-06-12 18:30:49 +0200

    Cleanup: rename Main/index.js -> Main/Main.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/css/v4-shims.css';
 
 import './index.scss';
-import Main from './components/Main';
+import Main from './components/Main/Main';
 // import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 
