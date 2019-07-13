fa4e4dd6b9fc3d0c5429044c87eda60a94616c43
Anze <anze@grafolean.com>
2019-07-13 21:14:08 +0200

    Cleanup: relocate /api/accounts/ endpoints to a separate file

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,4 +1,5 @@
 from .admin import admin_api
 from .profile import profile_api
+from .accounts import accounts_api
 
 from .common import noauth, auth_no_permissions, mqtt_publish_changed, MQTT_HOSTNAME, MQTT_PORT, MQTT_WS_HOSTNAME, MQTT_WS_PORT
