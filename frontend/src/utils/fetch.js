eb93921ac3e18bbd19acf8950b76695bce5c9243
Anze <anze@grafolean.com>
2019-10-04 21:42:35 +0200

    Fix fetch: safer removal of fetchIds, joined competing MQTT connections, nicer logging

--- frontend/src/utils/fetch.js
+++ frontend/src/utils/fetch.js
@@ -72,43 +72,51 @@ class MQTTFetcher {
     ...
   */
 
+  connectingToMqtt = null;
   mqttClient = null;
-  fetches = [];
+  fetches = {};
+  nextFetchId = 0;
 
   connect = (hostname, port, isSSL, jwtToken) => {
     if (!hostname) {
       return null;
     }
-    return new Promise((resolve, reject) => {
-      let notYetConnectedClient = new window.Paho.MQTT.Client(
-        hostname,
-        Number(port),
-        `grafolean-frontend-${VERSION_INFO.ciCommitTag || 'v?.?.?'}-${moment().format('x')}`,
-      );
-      notYetConnectedClient.onConnectionLost = responseObject => {
-        if (responseObject.errorCode !== 0) {
-          console.error('MQTT connection lost!'); // !!! handling?
-        }
-      };
-      notYetConnectedClient.onMessageArrived = this.onMessageReceived;
-      notYetConnectedClient.connect({
-        onSuccess: () => {
-          console.log('MQTT connected.');
-          this.mqttClient = notYetConnectedClient;
-          resolve();
-        },
-        onFailure: () => {
-          console.error('Error connecting to MQTT broker via WebSockets');
-          reject();
-        },
-        timeout: 5,
-        reconnect: true, // not sure how to control reconnect?
-        keepAliveInterval: 36000000,
-        userName: jwtToken,
-        password: 'can.be.empty',
-        useSSL: isSSL,
+    // there might be multiple requests to connection in parallel (before the first one succeeds), so we
+    // must make sure that only one connection is requested. We do this by caching the promise:
+    if (this.connectingToMqtt === null) {
+      this.connectingToMqtt = new Promise((resolve, reject) => {
+        let notYetConnectedClient = new window.Paho.MQTT.Client(
+          hostname,
+          Number(port),
+          `grafolean-frontend-${VERSION_INFO.ciCommitTag || 'v?.?.?'}-${moment().format('x')}`,
+        );
+        notYetConnectedClient.onConnectionLost = responseObject => {
+          if (responseObject.errorCode !== 0) {
+            console.error('MQTT connection lost!'); // !!! handling?
+          }
+        };
+        notYetConnectedClient.onMessageArrived = this.onMessageReceived;
+        notYetConnectedClient.connect({
+          onSuccess: () => {
+            console.log('MQTT connected.');
+            this.mqttClient = notYetConnectedClient;
+            resolve();
+          },
+          onFailure: () => {
+            console.error('Error connecting to MQTT broker via WebSockets');
+            reject();
+          },
+          timeout: 5,
+          reconnect: true, // not sure how to control reconnect?
+          keepAliveInterval: 36000000,
+          userName: jwtToken,
+          password: 'can.be.empty',
+          useSSL: isSSL,
+        });
       });
-    });
+    }
+
+    return this.connectingToMqtt;
   };
 
   isConnected = () => {
@@ -116,18 +124,24 @@ class MQTTFetcher {
   };
 
   start = (topic, queryParams, onFetchCallback, onErrorCallback, onNotification) => {
-    const newFetchId = this.fetches.length;
-    this.fetches.push({
+    const newFetchId = '' + this.nextFetchId;
+    this.nextFetchId += 1;
+    this.fetches[newFetchId] = {
       topic: topic,
       queryParams: queryParams,
       onFetchCallback: onFetchCallback,
       onErrorCallback: onErrorCallback,
       onNotification: onNotification,
       abortController: null,
-    });
-    this._doFetchHttp(newFetchId).finally(() => {
-      this._subscribe(newFetchId);
-    });
+    };
+    this._doFetchHttp(newFetchId)
+      .then(() => {
+        this._subscribe(newFetchId);
+      })
+      .catch(err => {
+        console.debug('Start failed for fetchId ' + newFetchId);
+      });
+    //
     return newFetchId;
   };
 
@@ -145,6 +159,7 @@ class MQTTFetcher {
         if (err.name !== 'AbortError') {
           this.fetches[fetchId].onErrorCallback(err);
         }
+        throw err;
       });
   };
 
@@ -152,14 +167,15 @@ class MQTTFetcher {
     // Note that unsubscribe is async so it might happen that some messages come *after* the unsubscribe:
     //   https://www.eclipse.org/paho/files/jsdoc/Paho.MQTT.Client.html
     // Since we can't rush the unsubscribing process, we simply ignore such messages (they match no known topic).
-    console.log('Message received:', message.destinationName, message.topic, message.payloadString);
+    console.debug('Message received:', message.destinationName, message.topic, message.payloadString);
     if (!message.destinationName.startsWith('changed/')) {
       console.error('Message doesn\'t start with "changed/", how did we get it?');
       return;
     }
     const changedTopic = message.destinationName.substring('changed/'.length);
-    this.fetches.forEach((f, fetchId) => {
-      if (f === null || f.topic !== changedTopic) {
+    Object.keys(this.fetches).forEach(fetchId => {
+      const f = this.fetches[fetchId];
+      if (f === undefined || f.topic !== changedTopic) {
         return;
       }
       try {
@@ -186,14 +202,17 @@ class MQTTFetcher {
       console.warn(`Not connected to MQTT, not subscribing`);
       return;
     }
+    if (!this.fetches[fetchId]) {
+      console.warn('Not subscribing, fetchId not there: ' + fetchId);
+      return;
+    }
     const { topic, onErrorCallback } = this.fetches[fetchId];
     this.mqttClient.subscribe(`changed/${topic}`, {
-      onSuccess: () => console.log('Successfully subscribed to topic: ' + topic),
+      onSuccess: () => console.debug('Successfully subscribed to topic: ' + topic),
       onFailure: () => {
         console.error('Error subscribing to topic: ' + topic);
         onErrorCallback('Error subscribing to topic: ' + topic);
-        //this.fetches.splice(fetchId, 1); // don't do this, other ids will change then...
-        this.fetches[fetchId] = null;
+        delete this.fetches[fetchId];
       },
     });
   };
@@ -206,7 +225,7 @@ class MQTTFetcher {
     const { topic } = this.fetches[fetchId];
     if (this.mqttClient) {
       // if client component stops listening before we even connected, this.mqttClient could be null
-      console.log('Unsubscribed from topic: ' + topic);
+      console.debug('Unsubscribed from topic: ' + topic);
       this.mqttClient.unsubscribe(topic);
     }
 
@@ -214,17 +233,18 @@ class MQTTFetcher {
     if (this.fetches[fetchId].abortController) {
       this.fetches[fetchId].abortController.abort();
     }
-    // now cut out the element at fetchId from the list:
-    //this.fetches.splice(fetchId, 1); // don't do this, other ids will change then...
-    this.fetches[fetchId] = null;
+    delete this.fetches[fetchId];
   };
 
   disconnect = () => {
+    const allFetchIds = Object.keys(this.fetches);
+    allFetchIds.forEach(fetchId => this.stop(fetchId));
     if (this.mqttClient) {
       this.mqttClient.disconnect();
     }
+    this.connectingToMqtt = null;
     this.mqttClient = null;
-    this.fetches = [];
+    this.fetches = {};
   };
 }
 
