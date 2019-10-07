3b36048c18e7e8d07174f47a5e79e80a7a9e2e2e
Anze <anze@grafolean.com>
2019-10-07 17:33:19 +0200

    Revert - LineChartCanvas will not be able to fetch its own data, because the same data is needed for example for showing tooltips

--- frontend/src/components/Widgets/GLeanChartWidget/LineChartCanvas.js
+++ frontend/src/components/Widgets/GLeanChartWidget/LineChartCanvas.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { compile } from 'mathjs';
 
 import { generateSerieColor } from './utils';
 import { PersistentFetcher } from '../../../utils/fetch/PersistentFetcher';
@@ -47,10 +46,6 @@ export class LineChartCanvases extends React.Component {
 }
 
 class _LineChartSingleCanvas extends React.PureComponent {
-  state = {
-    data: null,
-  };
-
   constructor(props) {
     super(props);
     this.canvasRef = React.createRef();
@@ -66,12 +61,8 @@ class _LineChartSingleCanvas extends React.PureComponent {
   }
 
   drawOnCanvas() {
-    const { data } = this.state;
-    if (data === null) {
-      return; // no data available yet
-    }
     const ctx = this.canvasContext;
-    const { fromTs, scale, isAggr } = this.props;
+    const { fromTs, scale } = this.props;
     const ts2x = ts => (ts - fromTs) * scale;
     ctx.clearRect(0, 0, this.canvasRef.current.width, this.canvasRef.current.height);
     // debugging:
@@ -79,69 +70,55 @@ class _LineChartSingleCanvas extends React.PureComponent {
     // ctx.rect(0, 0, this.canvasRef.current.width, this.canvasRef.current.height);
     // ctx.stroke();
     this.props.drawnChartSeries.forEach(cs => {
-      const mathExpression = compile(cs.expression);
-      if (!data.hasOwnProperty(cs.path)) {
-        return;
-      }
-      const v2y = this.props.yAxesProperties[cs.unit].derived.v2y;
-      const pathPoints = data[cs.path].data.map(d => ({
-        x: ts2x(d.t),
-        y: v2y(mathExpression.evaluate({ $1: d.v })),
-        minY: isAggr ? v2y(mathExpression.evaluate({ $1: d.minv })) : undefined,
-        maxY: isAggr ? v2y(mathExpression.evaluate({ $1: d.maxv })) : undefined,
-      }));
-      //pathPoints.sort((a, b) => (a.x < b.x ? -1 : 1)); // seems like the points weren't sorted by now... we should fix this properly
-      const linePoints = pathPoints.map(p => `${p.x},${p.y}`);
-      const serieColor = generateSerieColor(cs.path, cs.index);
-      ctx.strokeStyle = serieColor;
-      ctx.fillStyle = serieColor;
-
-      if (isAggr) {
+      this.props.intervals.forEach(interval => {
+        if (!interval.csData.hasOwnProperty(cs.chartSerieId)) {
+          return;
+        }
+        const v2y = this.props.yAxesProperties[cs.unit].derived.v2y;
+        const pathPoints = interval.csData[cs.chartSerieId].map(p => ({
+          x: ts2x(p.t),
+          y: v2y(p.v),
+          minY: v2y(p.minv),
+          maxY: v2y(p.maxv),
+        }));
+        //pathPoints.sort((a, b) => (a.x < b.x ? -1 : 1)); // seems like the points weren't sorted by now... we should fix this properly
+        const linePoints = pathPoints.map(p => `${p.x},${p.y}`);
         const areaMinPoints = pathPoints.map(p => `${p.x},${p.minY}`);
         const areaMaxPointsReversed = pathPoints.map(p => `${p.x},${p.maxY}`).reverse();
-        ctx.beginPath();
-        ctx.globalAlpha = 0.2;
-        const areaPath = new Path2D(`M${areaMinPoints.join('L')}L${areaMaxPointsReversed}Z`);
-        ctx.fill(areaPath);
-        ctx.globalAlpha = 1.0;
-      }
+        const serieColor = generateSerieColor(cs.path, cs.index);
+        ctx.strokeStyle = serieColor;
+        ctx.fillStyle = serieColor;
 
-      ctx.beginPath();
-      const linePath = new Path2D(`M${linePoints.join('L')}`);
-      ctx.stroke(linePath);
+        if (this.props.isAggr) {
+          ctx.beginPath();
+          ctx.globalAlpha = 0.2;
+          const areaPath = new Path2D(`M${areaMinPoints.join('L')}L${areaMaxPointsReversed}Z`);
+          ctx.fill(areaPath);
+          ctx.globalAlpha = 1.0;
+        }
 
-      pathPoints.forEach(p => {
         ctx.beginPath();
-        ctx.lineWidth = 0;
-        ctx.arc(p.x, p.y, 1, 0, 2 * Math.PI);
-        ctx.fill();
+        const linePath = new Path2D(`M${linePoints.join('L')}`);
+        ctx.stroke(linePath);
+
+        pathPoints.forEach(p => {
+          ctx.beginPath();
+          ctx.lineWidth = 0;
+          ctx.arc(p.x, p.y, 1, 0, 2 * Math.PI);
+          ctx.fill();
+        });
       });
     });
   }
 
   onNotification = (mqttPayload, topic) => {
-    // check if we should fetch new data:
-    const { fromTs, toTs, drawnChartSeries } = this.props;
-    if (mqttPayload.t < fromTs || mqttPayload.t > toTs) {
-      return false; // wrong time, ignore
-    }
-    const path = topic.substring('accounts/1/values/'.length);
-    if (!drawnChartSeries.find(cs => cs.path === path)) {
-      return false; // unknown path, ignore
-    }
-    // yes, trigger fetch:
-    return true;
+    console.log('GOT LOAD', mqttPayload, topic);
   };
-
   onFetchError = errorMsg => {
     console.error(errorMsg);
   };
-
   onUpdateData = json => {
-    // update data:
-    this.setState({
-      data: json.paths,
-    });
+    console.log(json);
   };
 
   render() {
