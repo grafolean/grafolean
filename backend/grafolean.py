f5160713e26cdeaf9f4b1fc8a3cdcd91b300cc00
Anze <anze@grafolean.com>
2019-10-22 21:22:38 +0200

    Publish swagger.yml

--- backend/grafolean.py
+++ backend/grafolean.py
@@ -188,7 +188,8 @@ def generate_api_docs(filename, api_version):
                 "Grafolean is designed API-first. Meaning, every functionality of the system is accessible through the API " \
                 "described below. This allows integration with external systems so that (given the permissions) they too can " \
                 "enter values, automatically modify entities, set up dashboards... Everything that can be done through frontend " \
-                "can also be achieved through API, as frontend is just an UI which uses the API. "
+                "can also be achieved through API, as frontend is just an UI which uses the API.\n\n" \
+                "This documentation is also available as [swagger API definition](./swagger.yml)."
         }
     )
 
