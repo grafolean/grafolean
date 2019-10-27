1c05bd3e9244d80d4f61e4169f1f6db99b6b7a91
Anze <anze@grafolean.com>
2019-10-27 10:14:31 +0100

    Help when bots are not entered yet

--- frontend/src/components/WelcomePage/index.js
+++ frontend/src/components/WelcomePage/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { PersistentFetcher } from '../../utils/fetch/PersistentFetcher';
 
 import Loading from '../Loading';
+import { havePermission } from '../../utils/fetch';
 
-export default class WelcomePage extends React.Component {
+class WelcomePage extends React.Component {
   state = {
     botsAvailable: null,
   };
@@ -19,6 +21,7 @@ export default class WelcomePage extends React.Component {
   renderContent() {
     const { botsAvailable } = this.state;
     const accountId = this.props.match.params.accountId;
+    const { user } = this.props;
 
     if (botsAvailable === null) {
       return <Loading />;
@@ -26,17 +29,29 @@ export default class WelcomePage extends React.Component {
 
     return (
       <>
-        <h3>Welcome!</h3>
-        {botsAvailable ? (
-          <p>
-            Use <Link to={`/accounts/${accountId}/bots`}>bots</Link> to post data and{' '}
-            <Link to={`/accounts/${accountId}/dashboards/new`}>dashboards</Link> to view it.
-          </p>
+        {!botsAvailable ? (
+          <>
+            <h3>Welcome to your account!</h3>
+            {havePermission(`/accounts/${accountId}/bots`, 'POST', user.permissions) ? (
+              <p>
+                Next step is sending some data to Grafolean. To do that, a "bot" needs to be set up - either
+                To send data to this account, you need to setup at least one{' '}
+                <Link to={`/accounts/${accountId}/bots`}>bot</Link>.
+              </p>
+            ) : (
+              <p>
+                The first step would be to set up bots, but you don't seem to have permissions to do that.
+                Please contact the account admin.
+              </p>
+            )}
+          </>
         ) : (
-          <p>
-            To send data to this account, you need to setup at least one{' '}
-            <Link to={`/accounts/${accountId}/bots`}>bot</Link>.
-          </p>
+          <>
+            <p>
+              Use <Link to={`/accounts/${accountId}/bots`}>bots</Link> to post data and{' '}
+              <Link to={`/accounts/${accountId}/dashboards/new`}>dashboards</Link> to view it.
+            </p>
+          </>
         )}
       </>
     );
@@ -52,3 +67,7 @@ export default class WelcomePage extends React.Component {
     );
   }
 }
+const mapStoreToProps = store => ({
+  user: store.user,
+});
+export default connect(mapStoreToProps)(WelcomePage);
