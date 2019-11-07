663ede45906eb9b50eee22de061ccd4c336ac866
Anze <anze@grafolean.com>
2019-11-07 20:35:20 +0100

    Fix design of disabled buttons

--- frontend/src/components/Button/index.js
+++ frontend/src/components/Button/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 export default class Button extends React.Component {
   render() {
-    const { isLoading, children, ...rest } = this.props;
-    return <button {...rest}>{isLoading ? <i className="fa fa-circle-o-notch fa-spin" /> : children}</button>;
+    const { isLoading, children, disabled, ...rest } = this.props;
+    return (
+      <button className={`${disabled && 'disabled'}`} {...rest}>
+        {isLoading ? <i className="fa fa-circle-o-notch fa-spin" /> : children}
+      </button>
+    );
   }
 }
