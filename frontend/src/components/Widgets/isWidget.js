6bf826f5487dc573e34e9f9f376d5ad46fa3a2c8
Anze <anze@grafolean.com>
2019-12-20 18:48:05 +0100

    Refactor widget title bar

--- frontend/src/components/Widgets/isWidget.js
+++ frontend/src/components/Widgets/isWidget.js
@@ -6,8 +6,6 @@ import { fetchAuth } from '../../utils/fetch';
 import store from '../../store';
 import { ROOT_URL, handleFetchErrors, onFailure, setFullscreenDibs } from '../../store/actions';
 
-import WidgetTitleBar from './WidgetTitleBar';
-
 import './index.scss';
 
 const mapStoreToProps = store => ({
@@ -17,46 +15,7 @@ const mapStoreToProps = store => ({
 const isWidget = WrappedComponent => {
   const wrappedComponent = class Widget extends React.Component {
     state = {
-      buttonRenders: [],
       isFullscreen: false,
-    };
-
-    componentDidMount() {
-      this.widgetSetButtons();
-    }
-
-    widgetSetButtons = () => {
-      const deleteButton = (
-        <i
-          className="fa fa-trash"
-          onClick={ev => {
-            ev.preventDefault();
-            if (window.confirm("Are you sure you want to delete this widget? This can't be undone!")) {
-              this.deleteWidget();
-            }
-          }}
-        />
-      );
-      const editButton = (
-        <Route
-          render={({ history }) => (
-            <i
-              className="fa fa-edit"
-              onClick={() =>
-                history.push(
-                  `/accounts/${this.props.match.params.accountId}/dashboards/view/${
-                    this.props.dashboardSlug
-                  }/widget/${this.props.widgetId}/edit`,
-                )
-              }
-            />
-          )}
-        />
-      );
-
-      this.setState({
-        buttonRenders: [editButton, deleteButton],
-      });
     };
 
     deleteWidget = () => {
@@ -91,21 +50,59 @@ const isWidget = WrappedComponent => {
 
     render() {
       const { title, width, height, onPositionChange, isOnTop, isOnBottom, ...passThroughProps } = this.props;
+      const { isFullscreen } = this.state;
       const outerWidth = this.state.isFullscreen ? window.innerWidth : width;
       const outerHeight = this.state.isFullscreen ? window.innerHeight : height;
       const contentWidth = outerWidth - 42; // minus padding & border
       const contentHeight = outerHeight - 37 - 31; // minus padding, border & title bar height
       return (
         <div className={`widget ${this.state.isFullscreen ? 'fullscreen' : ''}`}>
-          <WidgetTitleBar
-            title={title}
-            buttonRenders={this.state.buttonRenders}
-            isFullscreen={this.state.isFullscreen}
-            onToggleFullscreen={this.toggleFullscreen}
-            isOnTop={isOnTop}
-            isOnBottom={isOnBottom}
-            onPositionChange={onPositionChange}
-          />
+          <div className="widget-title">
+            <h1>{title}</h1>
+
+            <span className="widget-button">
+              <Route
+                render={({ history }) => (
+                  <i
+                    className="fa fa-edit"
+                    onClick={() =>
+                      history.push(
+                        `/accounts/${this.props.match.params.accountId}/dashboards/view/${
+                          this.props.dashboardSlug
+                        }/widget/${this.props.widgetId}/edit`,
+                      )
+                    }
+                  />
+                )}
+              />
+            </span>
+
+            <span className="widget-button">
+              <i
+                className="fa fa-trash"
+                onClick={ev => {
+                  ev.preventDefault();
+                  if (window.confirm("Are you sure you want to delete this widget? This can't be undone!")) {
+                    this.deleteWidget();
+                  }
+                }}
+              />
+            </span>
+
+            <span className={`widget-button ${isOnTop ? 'disabled' : ''}`}>
+              <i className="fa fa-arrow-up" onClick={() => onPositionChange(-1)} />
+            </span>
+            <span className={`widget-button ${isOnBottom ? 'disabled' : ''}`}>
+              <i className="fa fa-arrow-down" onClick={() => onPositionChange(1)} />
+            </span>
+
+            <span className="widget-button">
+              <i
+                className={`fullscreen fa ${isFullscreen ? 'fa-compress' : 'fa-expand'}`}
+                onClick={() => this.toggleFullscreen(!isFullscreen)}
+              />
+            </span>
+          </div>
 
           <div className="widget-content">
             <WrappedComponent
@@ -113,7 +110,7 @@ const isWidget = WrappedComponent => {
               title={this.props.title}
               width={contentWidth}
               height={contentHeight}
-              isFullscreen={this.state.isFullscreen}
+              isFullscreen={isFullscreen}
               {...passThroughProps}
             />
           </div>
