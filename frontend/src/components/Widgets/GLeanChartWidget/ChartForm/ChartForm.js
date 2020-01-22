818c028c5c8afbc44eb4ed7fa66775d633dce5c5
Anze <anze@grafolean.com>
2020-01-22 19:43:27 +0100

    Move unit for field to a separate component (to be reused by other widgets' forms)

--- frontend/src/components/Widgets/GLeanChartWidget/ChartForm/ChartForm.js
+++ frontend/src/components/Widgets/GLeanChartWidget/ChartForm/ChartForm.js
@@ -3,32 +3,9 @@ import { compile } from 'mathjs';
 
 import MatchingPaths from './MatchingPaths';
 import Button from '../../../Button';
+import UnitFormField from '../../UnitFormField';
 
 import './ChartForm.scss';
-
-// const METRIC_PREFIXES = [
-//   { prefix: 'P', name: 'peta', power: 15 },
-//   { prefix: 'T', name: 'tera', power: 12 },
-//   { prefix: 'G', name: 'giga', power: 9 },
-//   { prefix: 'M', name: 'mega', power: 6 },
-//   { prefix: 'k', name: 'kilo', power: 3 },
-//   { prefix: 'h', name: 'hecto', power: 2 },
-//   { prefix: 'd', name: 'deci', power: 1 },
-//   { prefix: 'c', name: 'centi', power: -2 },
-//   { prefix: 'm', name: 'milli', power: -3 },
-//   { prefix: 'µ', name: 'micro', power: -6 },
-//   { prefix: 'n', name: 'nano', power: -9 },
-//   { prefix: 'p', name: 'pico', power: -12 },
-// ];
-const KNOWN_UNITS = {
-  '%': { name: 'percent', allowedPrefixes: '' },
-  '°C': { name: 'degrees Celcius', allowedPrefixes: '' },
-  s: { name: 'second', allowedPrefixes: 'mµnp' },
-  m: { name: 'meter', allowedPrefixes: 'pnµmcdk' },
-  bps: { name: 'bits per second', allowedPrefixes: 'kMGTP', kiloBase: 1024 },
-  B: { name: 'byte', allowedPrefixes: 'kMGTP', kiloBase: 1024 },
-  Bps: { name: 'bytes per second', allowedPrefixes: 'kMGTP', kiloBase: 1024 },
-};
 
 export default class ChartForm extends React.Component {
   static DEFAULT_SERIE_GROUP_CONTENT = {
@@ -70,33 +47,29 @@ export default class ChartForm extends React.Component {
     ev.preventDefault();
   };
 
-  getAllKnownUnits() {
-    let allUnits = Object.keys(KNOWN_UNITS).map(unit => ({
-      value: unit,
-      label: `${unit} (${KNOWN_UNITS[unit].name})`,
-      allowedPrefixes: KNOWN_UNITS[unit].allowedPrefixes,
-    }));
+  getOtherKnownUnits() {
+    let otherUnits = [];
     // we need to list all possible units, otherwise they won't be visible as selected options:
     for (let sg of this.props.content) {
       if (sg.unit === '') {
         continue;
       }
-      if (allUnits.find(u => u.value === sg.unit)) {
+      if (otherUnits.find(u => u.value === sg.unit)) {
         // we already know this unit, skip it
         continue;
       }
-      allUnits.push({
+      otherUnits.push({
         value: sg.unit,
         label: sg.unit,
         allowedPrefixes: null,
       });
     }
-    return allUnits;
+    return otherUnits;
   }
 
   render() {
-    const allUnits = this.getAllKnownUnits();
     const { content: seriesGroups, onChange, onBlur, setFieldValue } = this.props;
+    const otherKnownUnits = this.getOtherKnownUnits();
     return (
       <div className="chart-form">
         <div className="field">
@@ -151,29 +124,14 @@ export default class ChartForm extends React.Component {
                 </div>
               </div>
 
-              <div className="form-item field">
-                <label>Unit:</label>
-                <input
-                  type="text"
-                  value={sg.unit || ''}
-                  name={`content[${sgIndex}].unit`}
-                  onChange={onChange}
-                  onBlur={onBlur}
-                />
-                <p className="hint markdown">
-                  Commonly used units:
-                  {allUnits.map(u => (
-                    <span
-                      key={u.label}
-                      className="set-unit"
-                      onClick={() => setFieldValue(`content[${sgIndex}].unit`, u.value)}
-                      title={u.label}
-                    >
-                      {u.value}
-                    </span>
-                  ))}
-                </p>
-              </div>
+              <UnitFormField
+                value={sg.unit || ''}
+                name={`content[${sgIndex}].unit`}
+                otherKnownUnits={otherKnownUnits}
+                onChange={onChange}
+                onBlur={onBlur}
+                setFieldValue={setFieldValue}
+              />
             </div>
           ))}
           <Button onClick={this.handleAddEmptySerie}>+</Button>
