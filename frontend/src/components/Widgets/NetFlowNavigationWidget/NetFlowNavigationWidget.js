58b446bea8e1e7c21498fd0ef99e758b9ba3a505
Anze <anze@grafolean.com>
2020-03-05 20:44:44 +0100

    Fix NetFlow navigation widget - use 4h interval instead of 6h (doesn't exist)

--- frontend/src/components/Widgets/NetFlowNavigationWidget/NetFlowNavigationWidget.js
+++ frontend/src/components/Widgets/NetFlowNavigationWidget/NetFlowNavigationWidget.js
@@ -15,7 +15,7 @@ class NetFlowNavigationWidget extends React.Component {
   };
 
   DIRECTIONS = ['ingress', 'egress'];
-  INTERVALS = ['1min', '15min', '1h', '6h', '24h'];
+  INTERVALS = ['1min', '15min', '1h', '4h', '24h'];
   PATH_FILTER_ENTITIES = 'netflow.15min.ingress.entity.?';
   DEFAULT_DIRECTION = 'ingress';
   DEFAULT_INTERVAL = '1min';
