b5eba0fbbb94301ecf115e50ddd2157ed45d2692
Anze <anze@grafolean.com>
2020-03-15 14:52:08 +0100

    Add CORS support

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from api import CORS_DOMAINS
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_DOMAINS,
+    allow_credentials=False,  # indicates that cookies should be supported for cross-origin requests
+    allow_methods=['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
+    allow_headers=['Content-Type', 'Authorization'],
+    expose_headers=['X-JWT-Token'],
+    max_age=3600,
+)
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+@app.get("/accounts/{account_id}/values")
+async def read_root(account_id: int):
+    return {"Hello": "World2"}
 
 
 @app.get("/items/{item_id}")
