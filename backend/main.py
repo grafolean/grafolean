c436d3a48a0516e4763ce3f0a75f2a35b6d42b33
Anze <anze@grafolean.com>
2020-03-15 12:17:58 +0100

    Add supervisor, uvicorn and fastapi

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
