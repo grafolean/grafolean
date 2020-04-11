a07d87dc9cccfed913be4755f596fd7b7f5fa41f
Anze <anze@grafolean.com>
2020-04-11 17:47:07 +0200

    Remove unused input from NetFlow Navigation Widget form

--- frontend/src/components/Widgets/NetFlowNavigationWidget/NetFlowNavigationWidgetForm.js
+++ frontend/src/components/Widgets/NetFlowNavigationWidget/NetFlowNavigationWidgetForm.js
@@ -1,21 +1,13 @@
 import React from 'react';
 
 export default class NetFlowNavigationWidgetForm extends React.Component {
-  static DEFAULT_FORM_CONTENT = {
-    whatever: '',
-  };
+  static DEFAULT_FORM_CONTENT = {};
 
   render() {
-    const { content, onChange, onBlur } = this.props;
-    if (!content || Object.keys(content).length === 0) {
-      return null;
-    }
-    const { whatever = '' } = content;
     return (
       <div className="netflow-navigation-form">
         <div className="field">
-          <label>Construct label:</label>
-          <input type="text" value={whatever} name={`content.whatever`} onChange={onChange} onBlur={onBlur} />
+          <p className="hint">This widget type has no further options.</p>
         </div>
       </div>
     );
