859f0a6b7b1f6e02983578bf9495f06ef3450c4a
Anze <anze@grafolean.com>
2020-06-21 10:40:58 +0200

    Frontend - send data to backend on first step of signup

--- frontend/src/components/SignupPage/SignupPage.js
+++ frontend/src/components/SignupPage/SignupPage.js
@@ -6,12 +6,17 @@ import { VERSION_INFO } from '../../VERSION';
 
 import '../auth-form-page.scss';
 import './SignupPage.scss';
+import { ROOT_URL } from '../../store/actions';
 
 class SignupPage extends React.Component {
   state = {
     formValues: {
       email: '',
+      agree: false,
     },
+    processing: false,
+    errorMsg: '',
+    successfully_posted: false,
   };
 
   changeFormValue = ev => {
@@ -32,14 +37,31 @@ class SignupPage extends React.Component {
       errorMsg: undefined,
     });
 
-    await new Promise((resolve, reject) => {
-      setTimeout(() => {
-        this.setState({
-          errorMsg: '',
-          processing: false,
-        });
-        resolve();
-      }, 2000);
+    const {
+      formValues: { email, agree },
+    } = this.state;
+    const params = JSON.stringify({
+      email: email,
+      agree: agree,
+    });
+    const response = await fetch(`${ROOT_URL}/persons/signup/new`, {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: params,
+    });
+    if (!response.ok) {
+      const errorMsg = await response.text();
+      this.setState({
+        errorMsg: errorMsg,
+        processing: false,
+      });
+      return;
+    }
+
+    this.setState({
+      errorMsg: '',
+      processing: false,
+      successfully_posted: true,
     });
   };
 
@@ -48,6 +70,7 @@ class SignupPage extends React.Component {
       formValues: { email, agree },
       processing,
       errorMsg,
+      successfully_posted,
     } = this.state;
     return (
       <div className="signup-page auth-form-page">
@@ -56,40 +79,50 @@ class SignupPage extends React.Component {
             <img className="grafolean-logo" src="/grafolean.svg" alt="Grafolean" />
           </div>
 
-          <div className="form">
-            <h3>Create your account</h3>
+          {!successfully_posted ? (
+            <div className="form">
+              <h3>Create your account</h3>
 
-            <div className="field">
-              <label>E-mail:</label>
-              <input type="text" name="email" value={email} onChange={this.changeFormValue} autoFocus />
-            </div>
-
-            <div className="field agree">
-              <input type="checkbox" name="agree" value={agree} onChange={this.changeFormValue} />
-              <label htmlFor="agree">
-                I agree to the{' '}
-                <a href="https://grafolean.com/terms" target="_blank" rel="noopener noreferrer">
-                  Terms of Use
-                </a>
-              </label>
-            </div>
-
-            <Button type="submit" isLoading={processing} disabled={email.length === 0 || !agree}>
-              Create account
-            </Button>
-
-            {errorMsg && (
-              <div className="error-msg">
-                <i className="fa fa-exclamation-triangle" />
-                &nbsp;
-                {errorMsg}
+              <div className="field">
+                <label>E-mail:</label>
+                <input type="text" name="email" value={email} onChange={this.changeFormValue} autoFocus />
               </div>
-            )}
 
-            <div className="signup-text">
-              Already have an account? <Link to="/">Login.</Link>
+              <div className="field agree">
+                <input type="checkbox" name="agree" value={agree} onChange={this.changeFormValue} />
+                <label htmlFor="agree">
+                  I agree to the{' '}
+                  <a href="https://grafolean.com/terms" target="_blank" rel="noopener noreferrer">
+                    Terms of Use
+                  </a>
+                </label>
+              </div>
+
+              <Button type="submit" isLoading={processing} disabled={email.length === 0 || !agree}>
+                Create account
+              </Button>
+
+              {errorMsg && (
+                <div className="error-msg">
+                  <i className="fa fa-exclamation-triangle" />
+                  &nbsp;
+                  {errorMsg}
+                </div>
+              )}
+
+              <div className="signup-text">
+                Already have an account? <Link to="/">Login.</Link>
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className="form">
+              <h3>Confirm e-mail address</h3>
+
+              <div className="field">
+                An e-mail was sent to your address (<b>{email}</b>). Check your e-mail to continue!
+              </div>
+            </div>
+          )}
         </form>
 
         <div className="version">Grafolean version: {VERSION_INFO.ciCommitTag || 'unknown'}</div>
