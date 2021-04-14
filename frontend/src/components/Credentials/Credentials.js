fd3f7a8dc30c616d57b451eb95a0256bf2efaadb
Anze <anze@grafolean.com>
2021-04-14 22:01:40 +0200

    Refactor: convert Credentials to a function component

--- frontend/src/components/Credentials/Credentials.js
+++ frontend/src/components/Credentials/Credentials.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { fetchAuth } from '../../utils/fetch';
@@ -9,98 +9,88 @@ import LinkButton from '../LinkButton/LinkButton';
 import Loading from '../Loading';
 import Button from '../Button';
 
-export default class Credentials extends React.Component {
-  state = {
-    credentials: null,
-    fetchError: false,
+export default function Credentials(props) {
+  const accountId = props.match.params.accountId;
+  const [credentials, setCredentials] = useState(null);
+  const [fetchError, setFetchError] = useState(false);
+
+  const onCredentialsUpdate = credentials => {
+    setCredentials(credentials.list);
+    setFetchError(false);
   };
 
-  onCredentialsUpdate = credentials => {
-    this.setState({
-      credentials: credentials.list,
-      fetchError: false,
-    });
+  const onCredentialsUpdateError = errMsg => {
+    setCredentials([]);
+    setFetchError(true);
   };
 
-  onCredentialsUpdateError = errMsg => {
-    this.setState({
-      credentials: [],
-      fetchError: true,
-    });
-  };
-
-  performDelete = (ev, credId) => {
+  const performDelete = (ev, credId) => {
     ev.preventDefault();
 
-    const cred = this.state.credentials.find(cred => cred.id === credId);
+    const cred = credentials.find(cred => cred.id === credId);
     if (
       !window.confirm(`Are you sure you want to delete credentials "${cred.name}" ? This can't be undone!`)
     ) {
       return;
     }
 
-    fetchAuth(`${ROOT_URL}/accounts/${this.props.match.params.accountId}/credentials/${credId}`, {
+    fetchAuth(`${ROOT_URL}/accounts/${accountId}/credentials/${credId}`, {
       method: 'DELETE',
     });
   };
 
-  render() {
-    const { credentials, fetchError } = this.state;
-    const accountId = this.props.match.params.accountId;
+  return (
+    <div className="credentials frame">
+      <PersistentFetcher
+        resource={`accounts/${accountId}/credentials`}
+        onUpdate={onCredentialsUpdate}
+        onError={onCredentialsUpdateError}
+      />
 
-    return (
-      <div className="credentials frame">
-        <PersistentFetcher
-          resource={`accounts/${accountId}/credentials`}
-          onUpdate={this.onCredentialsUpdate}
-          onError={this.onCredentialsUpdateError}
-        />
-
-        {credentials === null ? (
-          <Loading />
-        ) : fetchError ? (
-          <>
-            <i className="fa fa-exclamation-triangle" /> Error fetching credentials
-          </>
-        ) : (
-          <>
-            {credentials.length > 0 && (
-              <table className="list">
-                <tbody>
-                  <tr>
-                    <th>Type</th>
-                    <th>Name</th>
-                    <th>Details</th>
-                    <th />
-                    <th />
+      {credentials === null ? (
+        <Loading />
+      ) : fetchError ? (
+        <>
+          <i className="fa fa-exclamation-triangle" /> Error fetching credentials
+        </>
+      ) : (
+        <>
+          {credentials.length > 0 && (
+            <table className="list">
+              <tbody>
+                <tr>
+                  <th>Type</th>
+                  <th>Name</th>
+                  <th>Details</th>
+                  <th />
+                  <th />
+                </tr>
+                {credentials.map(cred => (
+                  <tr key={cred.id}>
+                    <td>{cred.protocol}</td>
+                    <td>{cred.name}</td>
+                    <td>/</td>
+                    <td>
+                      <LinkButton title="Edit" to={`/accounts/${accountId}/credentials/edit/${cred.id}`}>
+                        <i className="fa fa-pencil" /> Edit
+                      </LinkButton>
+                    </td>
+                    <td>
+                      <Button className="red" onClick={ev => performDelete(ev, cred.id)}>
+                        <i className="fa fa-trash" /> Delete
+                      </Button>
+                    </td>
                   </tr>
-                  {credentials.map(cred => (
-                    <tr key={cred.id}>
-                      <td>{cred.protocol}</td>
-                      <td>{cred.name}</td>
-                      <td>/</td>
-                      <td>
-                        <LinkButton title="Edit" to={`/accounts/${accountId}/credentials/edit/${cred.id}`}>
-                          <i className="fa fa-pencil" /> Edit
-                        </LinkButton>
-                      </td>
-                      <td>
-                        <Button className="red" onClick={ev => this.performDelete(ev, cred.id)}>
-                          <i className="fa fa-trash" /> Delete
-                        </Button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )}
+                ))}
+              </tbody>
+            </table>
+          )}
 
-            <Link className="button green" to={`/accounts/${accountId}/credentials/new`}>
-              <i className="fa fa-plus" /> Add credentials
-            </Link>
-          </>
-        )}
-      </div>
-    );
-  }
+          <Link className="button green" to={`/accounts/${accountId}/credentials/new`}>
+            <i className="fa fa-plus" /> Add credentials
+          </Link>
+        </>
+      )}
+    </div>
+  );
 }
