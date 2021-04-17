0eb5173c8a7f7c1c2ad9b29ef1f36dfee2d6a087
Anze <anze@grafolean.com>
2021-04-17 13:47:32 +0200

    Add table filtering to dashboards

--- frontend/src/components/Dashboards/Dashboards.js
+++ frontend/src/components/Dashboards/Dashboards.js
@@ -5,6 +5,8 @@ import { fetchAuth } from '../../utils/fetch';
 import { PersistentFetcher } from '../../utils/fetch/PersistentFetcher';
 import { ROOT_URL } from '../../store/actions';
 import { useTableSort } from '../../utils/useTableSort';
+import { useTableFilter } from '../../utils/useTableFilter';
+import TableFilterInput from '../../utils/TableFilterInput';
 
 import Loading from '../Loading';
 import Button from '../Button';
@@ -13,12 +15,14 @@ const DEFAULT_SORT_ORDER = [
   ['name', true],
   ['id', true],
 ];
+const FILTERABLE_FIELDS = ['name'];
 
 function Dashboards(props) {
   const accountId = props.match.params.accountId;
   const [dashboards, setDashboards] = useState(null);
   const [fetchError, setFetchError] = useState(false);
   const [firstSortKey, firstSortDirection, applySortFunc, sortCompareFunc] = useTableSort(DEFAULT_SORT_ORDER);
+  const [filterTableFunc, filter, setFilter] = useTableFilter(FILTERABLE_FIELDS);
 
   const onRecordsUpdate = dashboards => {
     setDashboards(dashboards.list);
@@ -69,25 +73,30 @@ function Dashboards(props) {
                     Name
                     {firstSortKey === 'name' && <i className={`fa fa-sort-${firstSortDirection}`} />}
                   </th>
-                  <th />
+                  <th align="right">
+                    <TableFilterInput filter={filter} setFilter={setFilter} />
+                  </th>
                 </tr>
-                {dashboards.sort(sortCompareFunc).map(dashboard => (
-                  <tr key={dashboard.slug}>
-                    <td>
-                      <Link
-                        className="button green"
-                        to={`/accounts/${accountId}/dashboards/view/${dashboard.slug}`}
-                      >
-                        <i className="fa fa-bar-chart" /> {dashboard.name}
-                      </Link>
-                    </td>
-                    <td>
-                      <Button className="red" onClick={ev => performDelete(ev, dashboard.slug)}>
-                        <i className="fa fa-trash" /> Delete
-                      </Button>
-                    </td>
-                  </tr>
-                ))}
+                {dashboards
+                  .filter(filterTableFunc)
+                  .sort(sortCompareFunc)
+                  .map(dashboard => (
+                    <tr key={dashboard.slug}>
+                      <td>
+                        <Link
+                          className="button green"
+                          to={`/accounts/${accountId}/dashboards/view/${dashboard.slug}`}
+                        >
+                          <i className="fa fa-bar-chart" /> {dashboard.name}
+                        </Link>
+                      </td>
+                      <td align="right">
+                        <Button className="red" onClick={ev => performDelete(ev, dashboard.slug)}>
+                          <i className="fa fa-trash" /> Delete
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
               </tbody>
             </table>
           )}
