19e6d4cf6b184344af6bab04d3e71045a87bdff9
Anze <anze@grafolean.com>
2021-04-17 14:28:42 +0200

    Table filtering design improvements

--- frontend/src/utils/TableFilterInput.js
+++ frontend/src/utils/TableFilterInput.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './TableFilterInput.scss';
+
 function TableFilterInput({ filter, setFilter }) {
   return (
     <div className="table-filter-input">
@@ -8,6 +10,7 @@ function TableFilterInput({ filter, setFilter }) {
         value={filter}
         onChange={ev => setFilter(ev.target.value)}
         placeholder="Filter table"
+        size="7" // allows <input> to have smaller width
       />
       <i className={`fa fa-close ${filter === '' ? 'disabled' : ''}`} onClick={() => setFilter('')} />
     </div>
